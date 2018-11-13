3d6ea15181e5d06d77f54df03947baa53937e8e5
dean morgan <dean.morgan@mymona.uwi.edu>
2018-11-13 23:47:47 +0000

    html working

---
+++ request.js
@@ -0,0 +1,9 @@
+window.onload = function (){
+let search_button=document.getElementById("search");
+ 
+ search_button.onclick= function(){ 
+ window.location.assign("https://dean-lab06-deanmorgan303.c9users.io/request.php?q=definition")
+ 
+ }
+ 
+}
